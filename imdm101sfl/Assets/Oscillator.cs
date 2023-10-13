9a66067366d2adf292e132d8872c03139c4cb9ce
sunglazed <skim11@terpmail.umd.edu>
2023-10-12 21:30:51 -0400

    made the sun rotate
    the sphere object is under pivot now

--- imdm101sfl/Assets/Oscillator.cs
+++ imdm101sfl/Assets/Oscillator.cs
@@ -10,23 +10,34 @@ public class NewBehaviourScript : MonoBehaviour
     float width; 
     float height;
 
+    bool down;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2; 
-        width = 20; 
-        height = 10;  
+        speed = 2;
+        width = 20;
+        height = 10;
+        down = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       timecounter += Time.deltaTime * speed;
-
-       float x = Mathf.Cos (timecounter) * width;
-       float y = Mathf.Sin (timecounter) * height;
-       float z = 0;
-
-       transform.position = new Vector3 (x, y, z);
+        timecounter += Time.deltaTime * speed;
+        Debug.Log(timecounter);
+        float x = Mathf.Cos (timecounter) * width;
+        float y = Mathf.Sin (timecounter) * height;
+        float z = 0;
+        if(Input.GetKeyUp (KeyCode.L) && down == false)
+        {
+            transform.position = new Vector3 (x, Mathf.Sin(180) * height, z);
+            down = true;
+        }
+        if(Input.GetKeyUp (KeyCode.L) && down == true)
+        {
+            transform.position = new Vector3 (x, Mathf.Sin(360) * height, z);
+            down = false;
+        }
+        //transform.position = new Vector3 (x, y, z);
     }
 }
