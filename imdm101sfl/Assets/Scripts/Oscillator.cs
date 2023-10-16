2df7573e0683c106f7f1d527406b385b5e57d2c6
sunglazed <skim11@terpmail.umd.edu>
2023-10-16 19:30:46 -0400

    fixed bird

--- imdm101sfl/Assets/Scripts/Oscillator.cs
+++ imdm101sfl/Assets/Scripts/Oscillator.cs
@@ -10,34 +10,22 @@ public class NewBehaviourScript : MonoBehaviour
     float width; 
     float height;
 
-    bool down;
     // Start is called before the first frame update
     void Start()
     {
         speed = 2;
         width = 20;
         height = 10;
-        down = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         timecounter += Time.deltaTime * speed;
-        Debug.Log(timecounter);
         float x = Mathf.Cos (timecounter) * width;
         float y = Mathf.Sin (timecounter) * height;
         float z = 0;
-        if(Input.GetKeyUp (KeyCode.L) && down == false)
-        {
-            transform.position = new Vector3 (x, Mathf.Sin(180) * height, z);
-            down = true;
-        }
-        if(Input.GetKeyUp (KeyCode.L) && down == true)
-        {
-            transform.position = new Vector3 (x, Mathf.Sin(360) * height, z);
-            down = false;
-        }
-        //transform.position = new Vector3 (x, y, z);
+
+        transform.position = new Vector3(x,y,z);
     }
 }
